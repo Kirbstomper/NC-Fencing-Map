e9f29c2916c79c9b1e0e28fefe7d55a1542fc574
Chris Smith <chris.smith@ahead.com>
2023-01-02 10:53:53 -0700

    Initial Commit

---
+++ src/main/java/com/kirbstomper/ncfencingmap/NcFencingMapApplication.java
@@ -0,0 +1,13 @@
+package com.kirbstomper.ncfencingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcFencingMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcFencingMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirbstomper/ncfencingmap/NcFencingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirbstomper.ncfencingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcFencingMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
