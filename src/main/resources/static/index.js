517c35b651ad4602ec30b89ae8c26bd212c86e02
Chris Smith <chris.smith@ahead.com>
2023-01-02 13:45:31 -0700

    Add map visualization

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,15 @@
+function initMap() {
+    const myLatLng = { lat:35.99667277700945, lng:-80.35746978253349};
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatLng,
+    });
+  
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Hello World!",
+    });
+  }
+  
+  window.initMap = initMap;
