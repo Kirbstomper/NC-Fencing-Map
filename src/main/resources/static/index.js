07d14bcb1574cfe348cea149a33e5ffe43ffbda0
Chris Smith <chris.smith@ahead.com>
2023-01-03 09:21:23 -0700

    Add some basic formatting for information

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,15 +1,46 @@
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
 function initMap() {
-    const myLatLng = { lat:35.99667277700945, lng:-80.35746978253349};
+    const NC = { lat:35.62593076963908, lng:-79.3111004143203};
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,
+      zoom: 10,
+      center: NC,
     });
-  
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
+
+    fetch('/api/getEvents')
+    .then((response) => response.json())
+    .then((data) => data.forEach(element => {
+        console.log(element)
+
+        //Create the string for use in the info box. FORMATTED
+        var header = "<h1>" + element.eventName + "</h1>"
+        var address ="<p>" + element.address + "</p>"
+        var clubs = "<ul>"
+        element.clubs.forEach(club => {
+            clubs = clubs + "<li>" + club + "</li>"
+        })
+        clubs = clubs + "</ul>"
+    
+        var content_string = header + '\n' + address + "\n" + clubs
+        
+        const infowindow = new google.maps.InfoWindow({
+            content: content_string,
+            ariaLabel: "information",
+          });
+
+        const marker = new google.maps.Marker({
+            position: element.location,
+            map,
+            title: element.eventName,
+          });
+        
+          marker.addListener("click", () => {
+            infowindow.open({
+              anchor: marker,
+              map,
+            });
+          });
+    }));
   }
   
   window.initMap = initMap;
