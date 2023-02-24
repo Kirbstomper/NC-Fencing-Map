36c17165686ae0e441cc486040763d02ca8c82c1
Chris Smith <wingedkraby@gmail.com>
2023-02-24 17:35:28 -0500

    add default api-key

---
+++ src/main/java/com/kirbstomper/ncfencingmap/StartupInitialization.java
@@ -0,0 +1,40 @@
+package com.kirbstomper.ncfencingmap;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.kirbstomper.ncfencingmap.repositories.EventDataRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class StartupInitialization  {
+  private final EventDataRepository repository;
+
+  @EventListener
+  public void onApplicationEvent(ContextRefreshedEvent event) {
+    log.info("Increment counter");
+    
+    repository.deleteAll().block();
+    List<String> clubs = List.of("GSO", "FORGE","UNCC", "UNCG" );
+    List<String> address = List.of("5300 Sterling Forest Dr", "Greensboro Fencing Academy", "Forge Fencing Academy");
+    var counter = 0;
+    for(String add: address){
+      repository.save(new EventData.EventDataBuilder()
+      .address(add)
+      .clubs(clubs)
+      .eventName("Event: " + counter++)
+      .eventID(UUID.randomUUID().toString())
+      .location(GeoCoder.getBestLocation(add))
+      .build()).block();
+    }
+
+  }
+}
